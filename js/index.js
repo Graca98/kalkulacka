3072715a3329126a553f5f840d157394e4b0fdb5
Grácouš <84779291+Graca98@users.noreply.github.com>
2024-02-08 16:42:41 +0100

    (skoro)Funkční kalkulačka bez stylů
    Styly:
    Téměř žádný, zkusit udělat přes grid ve verzi 0.2
    
    JS:
    Podařilo se mi zprovoznit klikání operátorů přes tlačítká a zárověn na klávesnici. Další funkce blokuje každou klávesu krom čísel a operátoru + tab a backspace přes regex.
    
    input má nad sebou historii
    
    Přidaná funkce přímo do tlačítka (ušetřeno spoustu repetetivního kódu)

--- js/index.js
+++ js/index.js
@@ -12,21 +12,53 @@ function looseJsonParse(obj) {
     return eval?.(`"use strict";(${obj})`);
 }
 
-plus.onclick = () => {
-    momentalniHodnota += textPole.value + "+"
-    // vypisuje do labelu aktuální hodnotu
-    hodnota.innerHTML = momentalniHodnota
+// Zabranuje použití všech kláves krom čísel a +,-,*,...
+textPole.addEventListener('keydown', function(event) {
+    // Regulární výraz pro povolené znaky (čísla 0-9, +, -, *, /, %)
+    if (!/[0-9\+\-\*\/%]/.test(event.key) && event.key !== 'Backspace' && event.key !== 'Tab') {
+      event.preventDefault();
+    }
+});
+
+function pridejOperator(operator) {
+    if (textPole.value !== "") {
+        momentalniHodnota += textPole.value + operator
+        // vypisuje do labelu aktuální hodnotu
+        hodnota.innerHTML = momentalniHodnota
+        textPole.value = ""
+    }
+}
+
+function reset() {
+    momentalniHodnota = ""
+    hodnota.innerHTML = ""
     textPole.value = ""
 }
 
-minus.onclick = () => {
-    momentalniHodnota += textPole.value + "-"
-    // vypisuje do labelu aktuální hodnotu
-    hodnota.innerHTML = momentalniHodnota
-    textPole.value = ""
-}
+// plus.onclick = () => {
+//     momentalniHodnota += textPole.value + "+"
+//     // vypisuje do labelu aktuální hodnotu
+//     hodnota.innerHTML = momentalniHodnota
+//     textPole.value = ""
+// }
+
+// minus.onclick = () => {
+//     momentalniHodnota += textPole.value + "-"
+//     // vypisuje do labelu aktuální hodnotu
+//     hodnota.innerHTML = momentalniHodnota
+//     textPole.value = ""
+// }
 
 vysledek.onclick = () => {
+    if (textPole.value.match(/\+\-\*\//)) {
+        momentalniHodnota = textPole.value
+        hodnota.innerHTML = momentalniHodnota
+
+        let result = looseJsonParse(momentalniHodnota)
+        textPole.value = result
+        momentalniHodnota = ""
+    }
+
     momentalniHodnota += textPole.value
     hodnota.innerHTML = momentalniHodnota
 
